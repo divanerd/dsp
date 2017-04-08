eb5fc989c3060fdde5273c07af50ace9919422b2
divanerd <genamit@gmail.com>
2017-04-08 15:48:51 -0700

    Update advanced_python_dict.py

--- python/advanced_python_dict.py
+++ python/advanced_python_dict.py
@@ -1,1 +1,28 @@
-PLACE YOUR CODE HERE
+import csv
+from collections import Counter
+import re
+import itertools
+
+
+#6
+with open('faculty.csv') as csv_file:
+	reader = csv.DictReader(csv_file)
+ 	faculty_dict = {}
+ 	for row in reader:
+ 		faculty_dict[row['name'].rsplit(None, 1) [-1]] = [str(row['degree']), str(row['title']), str(row['email'])]
+# print(faculty_dict)
+
+top_three = dict(itertools.islice(faculty_dict.items(), 3))
+print(top_three)
+
+
+#7/8
+with open('faculty.csv') as csv_file:
+	reader = csv.DictReader(csv_file)
+	faculty_dict = {}
+	for row in reader:
+		faculty_dict[row['name'].rsplit(None, 1) [-1], row['name'].rsplit(None, 1) [0] ] = [str(row['degree']), str(row['title']), str(row['email'])]
+
+top_three = dict(itertools.islice(faculty_dict.items(), 3))
+print(top_three)
+
