08af0633d90846410eef0ddfe9f40071754058d5
divanerd <genamit@gmail.com>
2017-04-08 14:09:27 -0700

    Update advanced_python_csv.py

--- python/advanced_python_csv.py
+++ python/advanced_python_csv.py
@@ -1,1 +1,16 @@
-PLACE YOUR CODE HERE
+import csv
+from collections import Counter
+import re
+
+#write emails files to csv
+with open('faculty.csv') as csv_file:
+	reader = csv.DictReader(csv_file)
+	regex = re.compile(r'''@\(\w*.\){0,2}\w*.\w*\)''')
+	#regex = re.compile()
+	res=[]
+	with open('faculty_emails.csv', 'w') as f:
+		for row in reader:
+			f.write(row['email'])
+			f.write('\n')
+			print(row['email'])
+		f.close
