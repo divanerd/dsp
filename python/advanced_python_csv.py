c7d3ab1c9882eaf88160430116958fd097595113
divanerd <genamit@gmail.com>
2017-04-08 14:10:15 -0700

    Update advanced_python_csv.py

--- python/advanced_python_csv.py
+++ python/advanced_python_csv.py
@@ -5,9 +5,6 @@ import re
 #write emails files to csv
 with open('faculty.csv') as csv_file:
 	reader = csv.DictReader(csv_file)
-	regex = re.compile(r'''@\(\w*.\){0,2}\w*.\w*\)''')
-	#regex = re.compile()
-	res=[]
 	with open('faculty_emails.csv', 'w') as f:
 		for row in reader:
 			f.write(row['email'])
